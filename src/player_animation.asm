206184ee4168b4e48670aeb970f10ca8603b490c
agrellier <alberto.grellier@epitech.eu>
2020-03-21 20:12:40 +0100

    Add player walk animation

--- src/player_animation.asm
+++ src/player_animation.asm
@@ -1,8 +1,28 @@
+; Initialize the animations timer.
+; Params:
+;    None
+; Return:
+;    None
+; Registers:
+;    af -> Not preserved
+;    bc -> Not preserved
+;    de -> Not preserved
+;    hl -> Not preserved
 initAnimation::
 	ld hl, ANIMATION_TIMER
 	ld [hl], ANIMATION_TIME
 	ret
 
+; Update animations.
+; Params:
+;    None
+; Return:
+;    None
+; Registers:
+;    af -> Not preserved
+;    bc -> Not preserved
+;    de -> Not preserved
+;    hl -> Not preserved
 updateAnimation::
 	ld hl, ANIMATION_TIMER
 	ld a, 0
@@ -12,21 +32,65 @@ updateAnimation::
 	ret
 
 .animate
+	ld hl, PLAYER_STRUCT + BASIC_OBJECT_STRUCT_X_SPEED_OFF
+	ld a, 0
+	cp [hl]
+	jr z, .idle
+	call playerAnimationWalk
+	call initAnimation
+	ret
+
+.idle
 	call playerAnimationIdle
 	call initAnimation
 	ret
 
+; Animate player idle.
+; Params:
+;    None
+; Return:
+;    None
+; Registers:
+;    af -> Not preserved
+;    bc -> Not preserved
+;    de -> Not preserved
+;    hl -> Not preserved
 playerAnimationIdle::
 	ld hl, PLAYER_STRUCT + DISPLAYABLE_OBJECT_STRUCT_SPRITE
 	inc [hl]
 	inc [hl]
 	inc [hl]
 	inc [hl]
-	ld a, 0
+	ld a, PLAYER_SPRITE_NBR + 12
 	cp [hl]
-	jr z, .resetAnim
+	jr c, .resetIdleAnim
 	ret
 
-.resetAnim
+.resetIdleAnim
 	ld [hl], PLAYER_SPRITE_NBR
+	ret
+
+; Animate player walk.
+; Params:
+;    None
+; Return:
+;    None
+; Registers:
+;    af -> Not preserved
+;    bc -> Not preserved
+;    de -> Not preserved
+;    hl -> Not preserved
+playerAnimationWalk::
+	ld hl, PLAYER_STRUCT + DISPLAYABLE_OBJECT_STRUCT_SPRITE
+	inc [hl]
+	inc [hl]
+	inc [hl]
+	inc [hl]
+	ld a, PLAYER_SPRITE_WALK_NBR + 12
+	cp [hl]
+	jr z, .resetWalkAnim
+	ret
+
+.resetWalkAnim
+	ld [hl], PLAYER_SPRITE_WALK_NBR
 	ret
