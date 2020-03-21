fc241bf4e174fa75d1da679b704b928bcbc1fb31
Jonathan Martin <jonathan.martin@epitech.eu>
2020-03-21 21:01:20 +0100

    added better version of kick

--- src/btt_channel_four.asm
+++ src/btt_channel_four.asm
@@ -23,13 +23,13 @@ bttChannelFour::
     db $00, $00, $00, $C0, QUAVER ; NO SOUND
     db $00, $64, $02, $C0, QUAVER ; HiHat
 ;mesure 3
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
 ;mesure 4
     db $00, $82, $60, $C0, QUAVER / 2 ; Snare
@@ -49,74 +49,74 @@ bttChannelFour::
 
     db $00, $67, $07, $C0, QUAVER * 2 ; Tsssssss
 ;mesure 5
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
 ;mesure 6
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
 ;mesure 7
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
 ;mesure 8
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
 ;mesure 9
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
 ;mesure 10
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
 ;mesure 11
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
 ;mesure 12
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
@@ -141,70 +141,70 @@ bttChannelFour::
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
 ;mesure 15
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
 ;mesure 16
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
 ;mesure 17
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $82, $60, $C0, QUAVER ; Snare
     db $00, $64, $02, $C0, QUAVER ; HiHat
 ;mesure 18
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $00, $00, $C0, QUAVER ; NO SOUND
     db $00, $64, $02, $C0, QUAVER ; HiHat
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $00, $00, $C0, QUAVER ; NO SOUND
     db $00, $64, $02, $C0, QUAVER ; HiHat
 ;mesure 19
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $00, $00, $C0, QUAVER ; NO SOUND
     db $00, $64, $02, $C0, QUAVER ; HiHat
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $00, $00, $C0, QUAVER ; NO SOUND
     db $00, $64, $02, $C0, QUAVER ; HiHat
 ;mesure 20
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $00, $00, $C0, QUAVER ; NO SOUND
     db $00, $64, $02, $C0, QUAVER ; HiHat
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $00, $00, $C0, QUAVER ; NO SOUND
     db $00, $64, $02, $C0, QUAVER ; HiHat
 ;mesure 21
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $00, $00, $C0, QUAVER ; NO SOUND
     db $00, $64, $02, $C0, QUAVER ; HiHat
-    db $00, $84, $7F, $C0, QUAVER ; KICK
+    db $00, $C1, $7F, $C0, QUAVER ; KICK
     db $00, $64, $02, $C0, QUAVER ; HiHat
     db $00, $00, $00, $C0, QUAVER ; NO SOUND
     db $00, $64, $02, $C0, QUAVER ; HiHat
