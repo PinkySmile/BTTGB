0b2e7cefc09f2688b6c3477b2778d0792ec0cf68
Jonathan Martin <jonathan.martin@epitech.eu>
2020-03-21 14:04:08 +0100

    added first part of melody on channel one and intro label in channel two

---
+++ src/btt_channel_one.asm
@@ -0,0 +1,164 @@
+;channel 1
+bttChannelOne::
+;mesure 1
+	db $00, $80, $87, $14, $87, QUAVER + QUAVER / 2 ; DO#2
+	db $00, $80, $87, $14, $87, QUAVER + QUAVER / 2 ; DO#2
+	db $00, $80, $87, $14, $87, QUAVER + QUAVER / 2 ; DO#2
+	db $00, $80, $87, $14, $87, QUAVER + QUAVER / 2 ; DO#2
+	db $00, $80, $00, $14, $87, QUAVER ; NO SOUND
+	db $00, $80, $00, $2E, $87, QUAVER + QUAVER / 2 ; RE#2
+;mesure 2
+    db $00, $80, $87, $14, $87, QUAVER + QUAVER / 2 ; DO#2
+	db $00, $80, $87, $14, $87, QUAVER + QUAVER / 2 ; DO#2
+	db $00, $80, $87, $14, $87, QUAVER + QUAVER / 2 ; DO#2
+	db $00, $80, $87, $14, $87, QUAVER ; DO#2
+	db $00, $80, $00, $39, $87, QUAVER ; MI2
+	db $00, $80, $00, $2E, $87, QUAVER + QUAVER / 2 ; RE#2
+;mesure 3
+    db $00, $80, $87, $14, $87, QUAVER + QUAVER / 2 ; DO#2
+    db $00, $80, $87, $14, $87, QUAVER + QUAVER / 2 ; DO#2
+    db $00, $80, $87, $14, $87, QUAVER + QUAVER / 2 ; DO#2
+    db $00, $80, $87, $14, $87, QUAVER + QUAVER / 2 ; DO#2
+;mesure 4
+    db $00, $80, $07, $4F, $87, QUAVER * 7 + QUAVER / 2 ; FA#2
+    db $00, $80, $87, $14, $87, QUAVER ; DO#2
+    db $00, $80, $B7, $D6, $86, QUAVER ; LA1
+;mesure 5
+    db $00, $80, $87, $14, $87, QUAVER ; DO#2
+    db $00, $80, $B7, $4F, $87, QUAVER ; FA#2
+    db $00, $80, $87, $14, $87, QUAVER ; DO#2
+    db $00, $80, $B7, $D6, $86, QUAVER ; LA1
+    db $00, $80, $87, $14, $87, QUAVER ; DO#2
+    db $00, $80, $B7, $4F, $87, QUAVER ; FA#2
+    db $00, $80, $87, $14, $87, QUAVER ; DO#2
+    db $00, $80, $B7, $D6, $86, QUAVER ; LA1
+;mesure 6
+    db $00, $80, $87, $2E, $87, QUAVER ; RE#2
+    db $00, $80, $B7, $4F, $87, QUAVER ; FA#2
+    db $00, $80, $87, $2E, $87, QUAVER ; RE#2
+    db $00, $80, $B7, $F7, $86, QUAVER ; SI1
+    db $00, $80, $87, $2E, $87, QUAVER ; RE#2
+    db $00, $80, $B7, $4F, $87, QUAVER ; FA#2
+    db $00, $80, $87, $2E, $87, QUAVER ; RE#2
+    db $00, $80, $B7, $F7, $86, QUAVER ; SI1
+;mesure 7
+    db $00, $80, $87, $F7, $86, QUAVER ; SI1
+    db $00, $80, $B7, $39, $87, QUAVER ; MI2
+    db $00, $80, $87, $F7, $86, QUAVER ; SI1
+    db $00, $80, $B7, $C4, $86, QUAVER ; SOL#1
+    db $00, $80, $87, $F7, $86, QUAVER ; SI1
+    db $00, $80, $B7, $39, $87, QUAVER ; MI2
+    db $00, $80, $87, $F7, $86, QUAVER ; SI1
+    db $00, $80, $B7, $C4, $86, QUAVER ; SOL#1
+;mesure 8
+    db $00, $80, $87, $14, $87, QUAVER ; DO#2
+    db $00, $80, $B7, $39, $87, QUAVER ; MI2
+    db $00, $80, $87, $14, $87, QUAVER ; DO#2
+    db $00, $80, $B7, $D6, $86, QUAVER ; LA1
+    db $00, $80, $87, $14, $87, QUAVER ; DO#2
+    db $00, $80, $B7, $44, $87, QUAVER ; FA2
+    db $00, $80, $87, $14, $87, QUAVER ; DO#2
+    db $00, $80, $B7, $C4, $86, QUAVER ; SOL#1
+;mesure 9
+    db $00, $80, $87, $14, $87, QUAVER ; DO#2
+    db $00, $80, $B7, $4F, $87, QUAVER ; FA#2
+    db $00, $80, $87, $14, $87, QUAVER ; DO#2
+    db $00, $80, $B7, $D6, $86, QUAVER ; LA1
+    db $00, $80, $87, $14, $87, QUAVER ; DO#2
+    db $00, $80, $B7, $4F, $87, QUAVER ; FA#2
+    db $00, $80, $87, $14, $87, QUAVER ; DO#2
+    db $00, $80, $B7, $D6, $86, QUAVER ; LA1
+;mesure 10
+    db $00, $80, $87, $2E, $87, QUAVER ; RE#2
+    db $00, $80, $B7, $4F, $87, QUAVER ; FA#2
+    db $00, $80, $87, $2E, $87, QUAVER ; RE#2
+    db $00, $80, $B7, $F7, $86, QUAVER ; SI1
+    db $00, $80, $87, $2E, $87, QUAVER ; RE#2
+    db $00, $80, $B7, $4F, $87, QUAVER ; FA#2
+    db $00, $80, $87, $2E, $87, QUAVER ; RE#2
+    db $00, $80, $B7, $F7, $86, QUAVER ; SI1
+;mesure 11
+    db $00, $80, $87, $F7, $86, QUAVER ; SI1
+    db $00, $80, $B7, $39, $87, QUAVER ; MI2
+    db $00, $80, $87, $F7, $86, QUAVER ; SI1
+    db $00, $80, $B7, $C4, $86, QUAVER ; SOL#1
+    db $00, $80, $87, $F7, $86, QUAVER ; SI1
+    db $00, $80, $B7, $39, $87, QUAVER ; MI2
+    db $00, $80, $87, $F7, $86, QUAVER ; SI1
+    db $00, $80, $B7, $C4, $86, QUAVER ; SOL#1
+;mesure 12
+    db $00, $80, $87, $14, $87, QUAVER ; DO#2
+    db $00, $80, $B7, $39, $87, QUAVER ; MI2
+    db $00, $80, $87, $14, $87, QUAVER ; DO#2
+    db $00, $80, $B7, $D6, $86, QUAVER ; LA1
+    db $00, $80, $87, $14, $87, QUAVER ; DO#2
+    db $00, $80, $B7, $44, $87, QUAVER ; FA2
+    db $00, $80, $87, $14, $87, QUAVER ; DO#2
+    db $00, $80, $B7, $C4, $86, QUAVER ; SOL#1
+;mesure 13
+    db $00, $80, $B0, $4F, $87, QUAVER / 2 ; FA#2
+    db $00, $80, $B0, $62, $87, QUAVER / 2 ; SOL#2
+    db $00, $80, $B0, $6B, $87, QUAVER / 2 ; LA2
+    db $00, $80, $B0, $7C, $87, QUAVER ; SI2
+    db $00, $80, $B0, $8A, $87, QUAVER ; DO#3
+    db $00, $80, $B0, $9D, $87, QUAVER * 3 + QUAVER / 2 ; MI3
+    db $00, $80, $00, $9D, $87, QUAVER ; NO SOUND
+;mesure 14
+    db $00, $80, $B7, $44, $87, QUAVER / 2 ; FA2
+    db $00, $80, $B7, $4F, $87, QUAVER / 2 ; FA#2
+    db $00, $80, $B7, $39, $87, QUAVER / 2 ; MI2
+    db $00, $80, $B7, $14, $87, QUAVER / 2 ; DO#2
+    db $00, $80, $B7, $05, $87, QUAVER / 2 ; DO2
+    db $00, $80, $B7, $14, $87, QUAVER / 2 ; DO#2
+    db $00, $80, $B7, $F7, $86, QUAVER / 2 ; SI1
+    db $00, $80, $B7, $D6, $86, QUAVER / 2 ; LA1
+
+    db $00, $80, $B7, $9E, $86, QUAVER / 2 ; FA#1
+    db $00, $80, $B7, $D6, $86, QUAVER / 2 ; LA1
+    db $00, $80, $B7, $F7, $86, QUAVER / 2 ; SI1
+    db $00, $80, $B7, $D6, $86, QUAVER / 2 ; LA1
+    db $00, $80, $B7, $14, $87, QUAVER / 2 ; DO#2
+    db $00, $80, $B7, $F7, $86, QUAVER / 2 ; SI1
+    db $00, $80, $B7, $39, $87, QUAVER / 2 ; MI2
+    db $00, $80, $B7, $4F, $87, QUAVER / 2 ; FA#2
+;mesure 15
+    db $00, $80, $B7, $8A, $87, QUAVER + QUAVER / 2 ; DO#3
+    db $00, $80, $B7, $8A, $87, QUAVER + QUAVER / 2 ; DO#3
+    db $00, $80, $B7, $8A, $87, QUAVER + QUAVER / 2 ; DO#3
+    db $00, $80, $B7, $8A, $87, QUAVER + QUAVER / 2 ; DO#3
+    db $00, $80, $B7, $8A, $87, QUAVER * 2; DO#3
+;mesure 16
+    db $00, $80, $B7, $2E, $87, QUAVER / 2 ; RE#2
+    db $00, $80, $B7, $39, $87, QUAVER / 2 ; MI2
+    db $00, $80, $B7, $21, $87, QUAVER / 2 ; RE2
+    db $00, $80, $B7, $F7, $86, QUAVER / 2 ; SI1
+    db $00, $80, $B7, $E7, $86, QUAVER / 2 ; LA#1
+    db $00, $80, $B7, $F7, $86, QUAVER / 2 ; SI1
+    db $00, $80, $B7, $D6, $86, QUAVER / 2 ; LA1
+    db $00, $80, $B7, $B2, $86, QUAVER / 2 ; SOL1
+
+    db $00, $80, $B7, $73, $86, QUAVER / 2 ; MI1
+    db $00, $80, $B7, $B2, $86, QUAVER / 2 ; SOL1
+    db $00, $80, $B7, $D6, $86, QUAVER / 2 ; LA1
+    db $00, $80, $B7, $B2, $86, QUAVER / 2 ; SOL1
+    db $00, $80, $B7, $F7, $86, QUAVER / 2 ; SI1
+    db $00, $80, $B7, $D6, $86, QUAVER / 2 ; LA1
+    db $00, $80, $B7, $F7, $86, QUAVER / 2 ; SI1
+    db $00, $80, $B7, $21, $87, QUAVER / 2 ; RE2
+;mesure 17
+    db $00, $80, $B7, $14, $87, QUAVER + QUAVER / 2 ; DO#2
+    db $00, $80, $B7, $39, $87, QUAVER + QUAVER / 2 ; MI2
+    db $00, $80, $B0, $6B, $87, QUAVER + QUAVER / 2 ; LA2
+    db $00, $80, $B0, $59, $87, QUAVER + QUAVER / 2 ; SOL2
+    db $00, $80, $B7, $39, $87, QUAVER ; MI2
+    db $00, $80, $B7, $44, $87, QUAVER ; FA2
+;mesure 18
+    db $00, $80, $40, $4F, $87, QUAVER * 8 ; FA#2
+;mesure 19
+    db $00, $80, $40, $4F, $87, QUAVER * 8 ; FA#2
+;mesure 20
+    db $00, $80, $40, $39, $87, QUAVER * 8 ; MI2
+;mesure 21
+    db $00, $80, $40, $14, $87, QUAVER * 4 ; DO#2
+    db $00, $80, $40, $62, $87, QUAVER * 4 ; SOL#2
+    db $00, $80, $00, $62, $87, $FF ; SOL#2
