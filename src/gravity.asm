7bab72f0b1a2631cb9b55696d35c95a8bb6c66a6
agrellier <alberto.grellier@epitech.eu>
2020-03-21 16:11:08 +0100

    Fix gravity limit

--- src/gravity.asm
+++ src/gravity.asm
@@ -9,11 +9,11 @@
 ;    de -> Not preserved
 ;    hl -> Not preserved
 initGravity::
-    ld hl, GRAVITY_TIMER
-    ld [hl], GRAVITY_CONSTANT
-    ld hl, GRAVITY_LIMITER
-    ld [hl], 0
-    ret
+	ld hl, GRAVITY_TIMER
+	ld [hl], GRAVITY_CONSTANT
+	ld hl, GRAVITY_LIMITER
+	ld [hl], 0
+	ret
 
 ; Update the gravity.
 ; Params:
@@ -26,42 +26,37 @@ initGravity::
 ;    de -> Not preserved
 ;    hl -> Not preserved
 updateGravity::
-    ld a, 0
-    ld hl, GRAVITY_TIMER
-    cp a, [hl]
-    jr nz, .isFalling
+	ld a, 0
+	ld hl, GRAVITY_TIMER
+	cp a, [hl]
+	jr nz, .isFalling
 
-    ld [hl], GRAVITY_CONSTANT
-    ld a, [hl]
-    ld hl, GRAVITY_LIMITER
-    sub [hl]
-    ld [hl], a
+	ld [hl], GRAVITY_CONSTANT
+	ld a, [hl]
+	ld hl, GRAVITY_LIMITER
+	sub [hl]
+	ld [hl], a
 
-
-    ld hl, PLAYER_STRUCT + BASIC_OBJECT_STRUCT_Y_SPEED_OFF
-    inc [hl]
-    bit 7, a
-    jr z, .skipOverflow
-    ld a, 7 ; Max speed
-    cp [hl]
-    jp c, .overflow
-    ld hl, GRAVITY_LIMITER
-    inc [hl]
-.skipOverflow
-    ret
+	ld hl, PLAYER_STRUCT + BASIC_OBJECT_STRUCT_Y_SPEED_OFF
+	ld a, 7 ; Max speed
+	cp [hl]
+	jr z, .overflow
+	inc [hl]
+	ld hl, GRAVITY_LIMITER
+	inc [hl]
+	ret
 
 
 .isFalling:
-    dec [hl]
-    ret
+	dec [hl]
+	ret
 
 .capGravity:
-    ld [hl], 1
-    ret
+	ld [hl], 1
+	ret
 
 .overflow:
-    ld [hl], 7
-    ret
+	ret
 
 
 
