ef00f834ceb36fb7c9d53d68bfef25f02837090f
Anonymus Raccoon <tr.tristan-roux@outlook.fr>
2020-03-21 16:31:35 +0100

    Replacing the test map

--- src/assets.asm
+++ src/assets.asm
@@ -19,7 +19,19 @@ JAMLettersEnd::
 
 ; All the sprites needed by the game
 GameSprites::
-	incbin "assets/player/player_idle/player_idle1.cfx"
+	incbin "assets/tiles/n0default.fx"
+	incbin "assets/tiles/n1reaped_poster.fx"
+	incbin "assets/tiles/n2long.fx"
+	incbin "assets/tiles/n3loul.fx"
+	incbin "assets/tiles/n4brick.fx"
+	incbin "assets/tiles/n5vBrick.fx"
+	incbin "assets/tiles/s0ground.fx"
+	incbin "assets/tiles/s1one_way.fx"
+	incbin "assets/tiles/s2wall.fx"
 GameSpritesEnd::
+
+PlayerSprites::
+	incbin "assets/player/player_idle/player_idle1.cfx"
+PlayerSpritesEnd::
 
 include "src/palettes.asm"
