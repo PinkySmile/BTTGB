b8b3d97f6eddcf11487247cfc3ab96d0db69d697
Clément Le Bihan <clement.le-bihan@epitech.eu>
2020-03-21 18:53:33 +0100

    interpreting bytes is ok but seems to not find correct bytes

--- src/collisions.asm
+++ src/collisions.asm
@@ -11,35 +11,47 @@
 checkCollisionsUnderPlayer::
 	; if the tile where the player is is standing is solid
 	ld a, [MAP_PTR_L]
-	ld b, b
 	and TILE_IS_SOLID
-	ld b, b
+	rra
+	rra
+	rra
+	rra
 	dec a
-	ld b, b
+	call z, checkpoint
 	ret z
 	; tile under the player
 	ld a, [MAP_PTR_L + MAP_SIZE_X_OFF]
-	ld b, b
 	and TILE_IS_SOLID
-	ld b, b
+	rra
+	rra
+	rra
+	rra
 	dec a
-	ld b, b
+	call z, checkpoint
 	ret z
 	; tile under left the player
 	ld a, [MAP_PTR_L + MAP_SIZE_X_OFF - 1]
-	ld b, b
 	and TILE_IS_SOLID
-	ld b, b
+	rra
+	rra
+	rra
+    rra
 	dec a
-	ld b, b
+	call z, checkpoint
 	ret z
 	; tile under right the player
 	ld a, [MAP_PTR_L + MAP_SIZE_X_OFF + 1]
-	ld b, b
 	and TILE_IS_SOLID
-	ld b, b
+	rra
+	rra
+	rra
+	rra
 	dec a
+	call z, checkpoint
 	ret z
-	ld b, b
 	; if no collision
+	ret
+
+checkpoint::
+	ld b, b
 	ret
