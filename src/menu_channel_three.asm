c148bc65a29e54b7067f2f195f58fda44df7187b
Jonathan Martin <jonathan.martin@epitech.eu>
2020-03-22 18:51:43 +0100

    testing third channel add

--- src/menu_channel_three.asm
+++ src/menu_channel_three.asm
@@ -2,8 +2,421 @@
 
 menuChannelThree::
 ;mesure 1
-    db $80, $00, $40, $9E, $86, QUAVER + QUAVER / 2 ;FA#1
-    db $80, $00, $20, $9E, $86, QUAVER + QUAVER / 2 ;FA#1
-    db $80, $00, $20, $9E, $86, QUAVER + QUAVER / 2 ;FA#1
-    db $80, $00, $20, $9E, $86, QUAVER + QUAVER / 2 ;FA#1
-    db $80, $00, $20, $9E, $86, QUAVER * 2 ;FA#1
+    db $80, $00, $00, $9E, $86, MQUAVER * 4 ;NO SOUND
+    db $80, $00, $20, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $20, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $20, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $20, $51, $84, MQUAVER ; DO#0
+;mesure 2
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+;mesure 3
+    db $80, $00, $40, $3C, $85, MQUAVER * 2 ; FA#0
+    db $80, $00, $40, $DF, $83, MQUAVER * 2 ; SIB
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+;mesure 4
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+;mesure 5
+    db $80, $00, $40, $3C, $85, MQUAVER * 2 ; FA#0
+    db $80, $00, $40, $DF, $83, MQUAVER * 2 ; SIB
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+;mesure 6
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+
+    db $80, $00, $40, $58, $83, MQUAVER * 3 ; LAB
+    db $80, $00, $40, $58, $83, MQUAVER ; LAB
+;mesure 7
+    db $80, $00, $40, $DF, $83, MQUAVER * 4 ; SIB
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+;mesure 8
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+;mesure 9
+    db $80, $00, $40, $7F, $82, MQUAVER * 2 ; FA#B
+    db $80, $00, $40, $DF, $83, MQUAVER * 2 ; SIB
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+;mesure 10
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+;mesure 11
+    db $80, $00, $40, $3C, $85, MQUAVER * 2 ; FA#0
+    db $80, $00, $40, $DF, $83, MQUAVER * 2 ; SIB
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+;mesure 12
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+;mesure 13
+    db $80, $00, $40, $3C, $85, MQUAVER * 2 ; FA#0
+    db $80, $00, $40, $DF, $83, MQUAVER * 2 ; SIB
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+;mesure 14
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $58, $83, MQUAVER * 3 ; LAB
+    db $80, $00, $40, $58, $83, MQUAVER ; LAB
+;mesure 15
+    db $80, $00, $40, $DF, $83, MQUAVER * 4 ; SIB
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+;mesure 16
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $51, $84, MQUAVER / 2 ; DO#0
+    db $80, $00, $00, $9E, $86, MQUAVER / 2 ;NO SOUND
+    db $80, $00, $40, $51, $84, MQUAVER * 2 ; DO#0
+    db $80, $00, $40, $B8, $84, MQUAVER * 2 ; RE#0
+;mesure 17
+    db $80, $00, $40, $E6, $84, MQUAVER * 2 ; MI0
+    db $80, $00, $40, $3C, $85, MQUAVER * 2 ; FA#0
+    db $80, $00, $40, $70, $81, MQUAVER ; RE#B
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $3C, $85, MQUAVER ; FA#0
+    db $80, $00, $40, $AC, $85, MQUAVER ; LA0
+;mesure 18
+    db $80, $00, $40, $29, $86, MQUAVER ; DO#1
+    db $80, $00, $40, $AC, $85, MQUAVER ; LA0
+    db $80, $00, $40, $3C, $85, MQUAVER ; FA#0
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $14, $83, MQUAVER ; SOL#B
+    db $80, $00, $40, $17, $84, MQUAVER ; DO0
+    db $80, $00, $40, $B8, $84, MQUAVER ; RE#0
+    db $80, $00, $40, $8A, $85, MQUAVER ; SOL#0
+;mesure 19
+    db $80, $00, $40, $0C, $86, MQUAVER ; DO1
+    db $80, $00, $40, $8A, $85, MQUAVER ; SOL#0
+    db $80, $00, $40, $B8, $84, MQUAVER ; RE#0
+    db $80, $00, $40, $17, $84, MQUAVER ; DO0
+    db $80, $00, $40, $94, $80, MQUAVER ; DO#B
+    db $80, $00, $40, $14, $83, MQUAVER ; SOL#B
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $E6, $84, MQUAVER ; MI0
+;mesure 20
+    db $80, $00, $40, $8A, $85, MQUAVER ; SOL#0
+    db $80, $00, $40, $29, $86, MQUAVER ; DO#1
+    db $80, $00, $40, $73, $86, MQUAVER ; MI1
+    db $80, $00, $40, $C4, $86, MQUAVER ; SOL#1
+    db $80, $00, $40, $94, $80, MQUAVER ; DO#B
+    db $80, $00, $40, $14, $83, MQUAVER ; SOL#B
+    db $80, $00, $40, $51, $84, MQUAVER ; DO#0
+    db $80, $00, $40, $13, $85, MQUAVER ; FA0
+;mesure 21
+    db $80, $00, $40, $8A, $85, MQUAVER ; SOL#0
+    db $80, $00, $40, $29, $86, MQUAVER ; DO#1
+    db $80, $00, $40, $8A, $86, MQUAVER ; FA1
+    db $80, $00, $40, $C4, $86, MQUAVER ; SOL#1
+    db $80, $00, $40, $CD, $85, MQUAVER * 4 ; LA#0
+;mesure 22
+    db $80, $00, $40, $AC, $85, MQUAVER * 4 ; LA0
+    db $80, $00, $40, $8A, $85, MQUAVER * 4 ; SOL#0
+;mesure 23
+    db $80, $00, $40, $0C, $86, MQUAVER * 4 ; DO1
+    db $80, $00, $40, $29, $86, MQUAVER * 2 ; DO#1
+    db $80, $00, $40, $5C, $86, MQUAVER * 2 ; RE#1
+;mesure 24
+    db $80, $00, $40, $73, $86, MQUAVER * 2 ; MI1
+    db $80, $00, $40, $5C, $86, MQUAVER * 2 ; RE#1
+    db $80, $00, $40, $73, $86, MQUAVER * 2 ; MI1
+    db $80, $00, $40, $9E, $86, MQUAVER * 2 ; FA#1
+;mesure 25
+    db $80, $00, $40, $C4, $86, MQUAVER * 2 ; SOL#1
+    db $80, $00, $40, $E7, $86, MQUAVER * 2 ; LA#1
+    db $80, $00, $40, $DF, $83, MQUAVER ; SIB
+    db $80, $00, $40, $DF, $83, MQUAVER / 2 ; SIB
+    db $80, $00, $00, $DF, $83, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $DF, $83, MQUAVER / 2 ; SIB
+    db $80, $00, $00, $DF, $83, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $DF, $83, MQUAVER ; SIB
+;mesure 26
+    db $80, $00, $40, $DF, $83, MQUAVER / 2 ; SIB
+    db $80, $00, $00, $DF, $83, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $DF, $83, MQUAVER / 2 ; SIB
+    db $80, $00, $00, $DF, $83, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $DF, $83, MQUAVER ; SIB
+    db $80, $00, $40, $DF, $83, MQUAVER / 2 ; SIB
+    db $80, $00, $00, $DF, $83, MQUAVER / 2 ; NO SOUND
+
+    db $80, $00, $40, $DF, $83, MQUAVER / 2 ; SIB
+    db $80, $00, $00, $DF, $83, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $DF, $83, MQUAVER ; SIB
+    db $80, $00, $40, $DF, $83, MQUAVER / 2 ; SIB
+    db $80, $00, $00, $DF, $83, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $DF, $83, MQUAVER ; SIB
+;mesure 27
+    db $80, $00, $40, $DF, $83, MQUAVER / 2 ; SIB
+    db $80, $00, $00, $DF, $83, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $DF, $83, MQUAVER ; SIB
+    db $80, $00, $40, $DF, $83, MQUAVER / 2 ; SIB
+    db $80, $00, $00, $DF, $83, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $DF, $83, MQUAVER / 2 ; SIB
+    db $80, $00, $00, $DF, $83, MQUAVER / 2 ; NO SOUND
+
+    db $80, $00, $40, $E6, $84, MQUAVER ; MI0
+    db $80, $00, $40, $E6, $84, MQUAVER / 2 ; MI0
+    db $80, $00, $00, $E6, $84, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $E6, $84, MQUAVER / 2 ; MI0
+    db $80, $00, $00, $E6, $84, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $E6, $84, MQUAVER ; MI0
+;mesure 28
+    db $80, $00, $40, $E6, $84, MQUAVER / 2 ; MI0
+    db $80, $00, $00, $E6, $84, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $E6, $84, MQUAVER / 2 ; MI0
+    db $80, $00, $00, $E6, $84, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $E6, $84, MQUAVER ; MI0
+    db $80, $00, $40, $E6, $84, MQUAVER / 2 ; MI0
+    db $80, $00, $00, $E6, $84, MQUAVER / 2 ; NO SOUND
+
+    db $80, $00, $40, $E6, $84, MQUAVER / 2 ; MI0
+    db $80, $00, $00, $E6, $84, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $E6, $84, MQUAVER ; MI0
+    db $80, $00, $40, $E6, $84, MQUAVER / 2 ; MI0
+    db $80, $00, $00, $E6, $84, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $E6, $84, MQUAVER ; MI0
+;mesure 29
+    db $80, $00, $40, $E6, $84, MQUAVER / 2 ; MI0
+    db $80, $00, $00, $E6, $84, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $E6, $84, MQUAVER ; MI0
+    db $80, $00, $40, $E6, $84, MQUAVER / 2 ; MI0
+    db $80, $00, $00, $E6, $84, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $E6, $84, MQUAVER / 2 ; MI0
+    db $80, $00, $00, $E6, $84, MQUAVER / 2 ; NO SOUND
+
+    db $80, $00, $40, $B8, $84, MQUAVER ; RE#0
+    db $80, $00, $40, $B8, $84, MQUAVER / 2 ; RE#0
+    db $80, $00, $00, $B8, $84, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $B8, $84, MQUAVER / 2 ; RE#0
+    db $80, $00, $00, $B8, $84, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $B8, $84, MQUAVER ; RE#0
+;mesure 30
+    db $80, $00, $40, $B8, $84, MQUAVER / 2 ; RE#0
+    db $80, $00, $00, $B8, $84, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $B8, $84, MQUAVER / 2 ; RE#0
+    db $80, $00, $00, $B8, $84, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $B8, $84, MQUAVER ; RE#0
+    db $80, $00, $40, $B8, $84, MQUAVER / 2 ; RE#0
+    db $80, $00, $00, $B8, $84, MQUAVER / 2 ; NO SOUND
+
+    db $80, $00, $40, $B8, $84, MQUAVER / 2 ; RE#0
+    db $80, $00, $00, $B8, $84, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $B8, $84, MQUAVER ; RE#0
+    db $80, $00, $40, $B8, $84, MQUAVER / 2 ; RE#0
+    db $80, $00, $00, $B8, $84, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $B8, $84, MQUAVER ; RE#0
+;mesure 31
+    db $80, $00, $40, $B8, $84, MQUAVER / 2 ; RE#0
+    db $80, $00, $00, $B8, $84, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $B8, $84, MQUAVER ; RE#0
+    db $80, $00, $40, $B8, $84, MQUAVER / 2 ; RE#0
+    db $80, $00, $00, $B8, $84, MQUAVER / 2 ; NO SOUND
+    db $80, $00, $40, $B8, $84, MQUAVER / 2 ; RE#0
+    db $80, $00, $00, $B8, $84, MQUAVER / 2 ; NO SOUND
+
+    db $80, $00, $40, $8A, $85, MQUAVER * 4 ; SOL#0
+;mesure 32
+    db $80, $00, $40, $3C, $85, MQUAVER * 4 ; FA#0
+    db $80, $00, $40, $E6, $84, MQUAVER * 4 ; MI0
+;mesure 33
+    db $80, $00, $40, $B8, $84, MQUAVER * 4 ; RE#0
+    db $80, $00, $40, $3C, $85, MQUAVER ; FA#0
+    db $80, $00, $40, $8A, $85, MQUAVER ; SOL#0
+    db $80, $00, $40, $CD, $85, MQUAVER ; LA#0
+    db $80, $00, $40, $EF, $85, MQUAVER ; SI0
+;mesure 34
+    db $80, $00, $40, $29, $86, MQUAVER ; DO#1
+    db $80, $00, $40, $EF, $85, MQUAVER ; SI0
+    db $80, $00, $40, $29, $86, MQUAVER ; DO#1
+    db $80, $00, $40, $5C, $86, MQUAVER ; RE#1
+    db $80, $00, $40, $73, $86, MQUAVER ; MI1
+    db $80, $00, $40, $5C, $86, MQUAVER ; RE#1
+    db $80, $00, $40, $73, $86, MQUAVER ; MI1
+    db $80, $00, $40, $9E, $86, MQUAVER ; FA#1
+;mesure 35 - 36 - 37
+    db $80, $00, $40, $C4, $86, MQUAVER ; SOL#1
+    db $80, $00, $40, $E7, $86, MQUAVER ; LA#1
+    db $80, $00, $40, $F7, $86, MQUAVER ; SI1
+    db $80, $00, $40, $05, $87, MQUAVER ; DO2
+    db $80, $00, $40, $29, $86, MQUAVER * 16 ; DO#1
+    db $80, $00, $40, $3C, $85, MQUAVER ; FA#0
+    db $80, $00, $40, $8A, $85, MQUAVER ; SOL#0
+    db $80, $00, $40, $CD, $85, MQUAVER ; LA#0
+    db $80, $00, $40, $EF, $85, MQUAVER ; SI0
+;mesure 38
+    db $80, $00, $40, $29, $86, MQUAVER ; DO#1
+    db $80, $00, $40, $EF, $85, MQUAVER ; SI0
+    db $80, $00, $40, $29, $86, MQUAVER ; DO#1
+    db $80, $00, $40, $5C, $86, MQUAVER ; RE#1
+    db $80, $00, $40, $73, $86, MQUAVER ; MI1
+    db $80, $00, $40, $5C, $86, MQUAVER ; RE#1
+    db $80, $00, $40, $73, $86, MQUAVER ; MI1
+    db $80, $00, $40, $9E, $86, MQUAVER ; FA#1
+;mesure 39
+    db $80, $00, $40, $C4, $86, MQUAVER ; SOL#1
+    db $80, $00, $40, $E7, $86, MQUAVER ; LA#1
+    db $80, $00, $40, $F7, $86, MQUAVER ; SI1
+    db $80, $00, $40, $05, $87, MQUAVER ; DO2
+    db $80, $00, $40, $51, $84, MQUAVER * 2 ; DO#0
+    db $80, $00, $40, $DF, $83, MQUAVER * 2 ; SIB
+;mesure 40
+    db $80, $00, $40, $58, $83, MQUAVER * 2 ; LAB
+    db $80, $00, $40, $14, $83, MQUAVER * 2 ; SOL#B
+    db $80, $00, $40, $7F, $82, MQUAVER * 2 ; FA#B
+    db $80, $00, $40, $D5, $81, MQUAVER * 2 ; MIB
+;mesure 41 - 42
+    db $80, $00, $40, $70, $81, MQUAVER * 2 ; RE#B
+    db $80, $00, $40, $94, $80, MQUAVER * 2 ; DO#B
+    db $80, $00, $40, $D5, $81, MQUAVER * 8 ; MIB
+;mesure 43
+    db $80, $00, $40, $D5, $81, MQUAVER * 8 ; MIB
+;mesure 44
+    db $80, $00, $40, $70, $81, MQUAVER * 8 ; RE#B
+;mesure 45
+    db $80, $00, $40, $15, $81, MQUAVER * 8 ; REB
+;mesure 46
+    db $80, $00, $40, $94, $80, MQUAVER * 8 ; DO#B
+;mesure 47
+    db $80, $00, $40, $20, $80, MQUAVER * 8 ; DOB
+;mesure 48
+    db $80, $00, $40, $D5, $81, MQUAVER * 8 ; MIB
+;mesure 49
+    db $80, $00, $40, $94, $80, MQUAVER * 8 ; DO#B
+    db $80, $00, $40, $D5, $81, MQUAVER * 2 ; MIB
+    db $80, $00, $40, $D5, $81, MQUAVER / 3 * 2 ; MIB
+    db $80, $00, $40, $D5, $81, MQUAVER / 3 * 2 ; MIB
+    db $80, $00, $40, $D5, $81, MQUAVER / 3 * 2 ; MIB
+;mesure 50
+    db $80, $00, $40, $E6, $84, MQUAVER * 4 ; MI0
+    db $80, $00, $40, $73, $86, MQUAVER * 8 ; MI1
+;mesure 51
+    db $80, $00, $40, $70, $81, MQUAVER * 2 ; RE#B
+    db $80, $00, $40, $70, $81, MQUAVER / 3 * 2 ; RE#B
+    db $80, $00, $40, $70, $81, MQUAVER / 3 * 2 ; RE#B
+    db $80, $00, $40, $70, $81, MQUAVER / 3 * 2 ; RE#B
+;mesure 52
+    db $80, $00, $40, $B8, $84, MQUAVER * 4 ; RE#0
+    db $80, $00, $40, $42, $86, MQUAVER * 8 ; RE1
+;mesure 53
+    db $80, $00, $40, $94, $80, MQUAVER * 2 ; DO#B
+    db $80, $00, $40, $94, $80, MQUAVER / 3 * 2 ; DO#B
+    db $80, $00, $40, $94, $80, MQUAVER / 3 * 2 ; DO#B
+    db $80, $00, $40, $94, $80, MQUAVER / 3 * 2 ; DO#B
+;mesure 54
+    db $80, $00, $40, $51, $84, MQUAVER * 4 ; DO#0
+    db $80, $00, $40, $0C, $86, MQUAVER * 8 ; DO1
+;mesure 55 - 56
+    db $80, $00, $40, $51, $84, MQUAVER * 8 ; DO#0
+    db $80, $00, $40, $B8, $81, MQUAVER / 3 * 4 ; RE#0
